389f4da6a0bf603d876059832309ae94159d2e83
toto <56038211+PhucNChau@users.noreply.github.com>
2020-08-27 20:53:15 +0700

    updated 27/08/2020

---
+++ BrowserType.js
@@ -0,0 +1,14 @@
+function Browser()
+{
+    this.
+}
+
+function getBrowserName()
+{
+    var lsBrowser = navigator.userAgent;
+    if (lsBrowser.indexOf("MSIE") >= 0)
+    {
+        lsBrowser = "MSIE";
+    }
+
+}
